2a4aba6e7a65fe532385a7d000dd0ff2be709b22
SeikaHirori <SeikaHirori@outlook.com>
2023-03-05 15:23:51 -0800

    Setup Structs and README.md - let's get this :3

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,19 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var helloWorld:String = "Hello world. Part 1 :3"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Part_1_Overview(hello_world: $helloWorld)
+        Part_2_Implementation()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Part_2_Implementation: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world! Part 2 :3")
+        }
+        .padding()
+
+    }
+}
+
+struct Part_1_Overview: View {
+    @Binding var hello_world:String
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text(hello_world)
+        }
+        .padding()
     }
 }
