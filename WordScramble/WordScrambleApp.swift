e7e4a8473cfd0f97fdcb9361886290be40cdc34a
SeikaHirori <SeikaHirori@outlook.com>
2023-03-05 14:45:25 -0800

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Seika Hirori on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
